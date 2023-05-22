5d14a0cca2115bfd27c14e5504bc109a8655cc8f
Yuri Sokolov <7556847+migus88@users.noreply.github.com>
2023-05-23 01:30:47 +0300

    implemented immunity effect

--- Assets/Domains/Core/Domains/Game/Code/View/Entities/Player.cs
+++ Assets/Domains/Core/Domains/Game/Code/View/Entities/Player.cs
@@ -11,6 +11,19 @@ namespace Migs.Asteroids.Game.View.Entities
 
         [SerializeField] private Transform _projectileParent;
         [SerializeField] private GameObject _thrustersEffect;
+        [SerializeField] private float _immunityFlickerInterval;
+
+        private float _nextFlickerTime;
+        private bool _isImmuneToDamage;
+        
+        private void Update()
+        {
+            if (_isImmuneToDamage && Time.time > _nextFlickerTime)
+            {
+                ViewRenderer.enabled = !ViewRenderer.enabled;
+                _nextFlickerTime = Time.time + _immunityFlickerInterval;
+            }
+        }
 
         public void Stop()
         {
@@ -29,8 +42,9 @@ namespace Migs.Asteroids.Game.View.Entities
 
         public void SetDamageImmunity(bool isImmune)
         {
+            ViewRenderer.enabled = true;
             Rigidbody.detectCollisions = !isImmune;
-            // TODO: Animate
+            _isImmuneToDamage = isImmune;
         }
 
         public void SetDrag(float amount)
